9907b55b68aeef8dd6721685db546232baf57a3c
Jake Lauterstein <97852239+jakelauterstein@users.noreply.github.com>
2022-06-03 22:04:39 -0500

    added first pass of functionality to 'controllers' methods

---
+++ controllers/thought-controller.js
@@ -0,0 +1,53 @@
+const { Thought, User } = require('../models')
+
+const thoughtController = {
+    //add thought to User
+    addThought({ params, body }, res) {
+        console.log(body);
+        Thought.create(body)
+          .then(({ _id }) => {
+            return User.findOneAndUpdate(
+              { _id: params.userId },
+              { $push: { thgouhts: _id } },
+              { new: true }
+            );
+          })
+          .then(dbUserData => {
+            if (!dbUserData) {
+              res.status(404).json({ message: 'No User found with this id!' });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch(err => res.json(err));
+      },
+
+
+    // add reaction to thought
+    addReaction({ params, body }, res) {
+        Thought.findOneAndUpdate(
+          { _id: params.thoughtId },
+          { $push: { reactions: body } },
+          { new: true, runValidators: true }
+        )
+          .then(dbUserData => {
+            if (!dbUserData) {
+              res.status(404).json({ message: 'No user found with this id!' });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch(err => res.json(err));
+      },
+          // remove reaction
+          removeReaction({ params }, res) {
+            Thought.findOneAndUpdate(
+                { _id: params.thoughtId },
+                { $pull: { reactions: { reactionId: params.reactionId } } },
+                { new: true }
+            )
+                .then(dbUserData => res.json(dbUserData))
+                .catch(err => res.json(err));
+            },
+}
+
